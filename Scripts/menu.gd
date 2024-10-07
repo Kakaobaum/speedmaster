222b2fcf1d9ba672b915b6b2300caab1e2679111
Bruno <wi22b058@technikum-wien.at>
2024-10-07 16:16:53 +0200

    Fixes for keybinds and audio, also changed comment language
    Fix: Added await for sound playback on keybinds and fixed Enter keybind functionality

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -26,7 +26,7 @@ func _play_audio(audio_file: String) -> void:
 		audio_player.stream = audio_stream
 		audio_player.play()
 
-# Funktionen für Hover - Sounds
+# Funktionen für Hover-Sounds
 
 # Wenn der 'Start Game'-Button gehovered wird
 func _on_btn_start_game_hovered() -> void:
@@ -46,32 +46,35 @@ func _on_btn_settings_hovered() -> void:
 
 # Tastatureingaben für Shortcuts definieren
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):  # Spielstart auf Enter setzte
-		_on_btn_start_game_pressed()
-	elif event.is_action_pressed("tutorial_shortcut"):  # Tutorial auf T setzen
-		_on_btn_tutorial_pressed()
-	elif event.is_action_pressed("ui_cancel"):  # Spiel verlassen auf Escape setzen
-		_on_btn_exit_pressed()
-	elif event.is_action_pressed("settings_shortcut"):  # Einstellungen auf S setzen
-		_on_btn_settings_pressed()
-
+	if event.is_action_pressed("ui_accept"):  # Shortcut für "Enter"
+		_on_btn_start_game_pressed()  # Sound abspielen und die Funktion ausführen
+	elif event.is_action_pressed("tutorial_shortcut"):  # Shortcut für "T"
+		_on_btn_tutorial_pressed()  # Sound abspielen und die Funktion ausführen
+	elif event.is_action_pressed("ui_cancel"):  # Shortcut für "Escape"
+		_on_btn_exit_pressed()  # Sound abspielen und die Funktion ausführen
+	elif event.is_action_pressed("settings_shortcut"):  # Shortcut für "S"
+		_on_btn_settings_pressed()  # Sound abspielen und die Funktion ausführen
 
 # Funktion für den Start Game-Button
 func _on_btn_start_game_pressed() -> void:
 	_play_audio("res://Audio/starten.mp3")
+	await get_tree().create_timer(1.5).timeout  # Warte 1,5 Sekunden,damit der Sound zuerst abgespielt werden kann
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
 # Funktion für den Tutorial-Button
 func _on_btn_tutorial_pressed() -> void:
 	_play_audio("res://Audio/tutorial.mp3")
+	await get_tree().create_timer(1.5).timeout  # Warte 1,5 Sekunden,damit der Sound zuerst abgespielt werden kann
 	get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
 
 # Funktion für den Exit-Button
 func _on_btn_exit_pressed() -> void:
 	_play_audio("res://Audio/verlassen.mp3")
+	await get_tree().create_timer(1.5).timeout  # Warte 1,5 Sekunden,damit der Sound zuerst abgespielt werden kann
 	get_tree().quit()
 
 # Funktion für den Settings-Button
 func _on_btn_settings_pressed() -> void:
 	_play_audio("res://Audio/settings.mp3")
-	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
+	await get_tree().create_timer(1.5).timeout  # Warte 1,5 Sekunden
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn") # Warte 1,5 Sekunden,damit der Sound zuerst abgespielt werden kann
