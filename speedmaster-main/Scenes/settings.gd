fdbb2ac2706b74f6183e15081d60dacd6ad5c504
Stefan <131394134+Kakaobaum@users.noreply.github.com>
2025-01-07 18:05:07 +0100

    Delete speedmaster-main directory

--- speedmaster-main/Scenes/settings.gd
+++
@@ -1,7 +0,0 @@
-extends Node  # Attach to the parent node of the slider and label (Settings)
-
-@export var audio_clip: AudioStream  # Expose an audio clip for the test sound (optional)
-
-func _on_btn_menu_pressed() -> void:
-	# Navigate back to the main menu scene when the button is pressed
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
