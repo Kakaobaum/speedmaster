676fbb6f12a45dafe0f5b0635f409db64813a082
Stefan <131394134+Kakaobaum@users.noreply.github.com>
2025-01-19 12:19:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -46,9 +46,12 @@ function App() {
   /**
    * A ref to manage background music. Using `useRef` ensures
    * that the same HTMLAudioElement persists across renders.
+   * https://audiocdn.epidemicsound.com/ES_ITUNES/6EHqvP_Nocturne/ES_Nocturne.mp3 - guitar
+   * https://audiocdn.epidemicsound.com/lqmp3/01HXV7VRJ2AZFK25CF4M6F6WC2.mp3 - ocean waves
+   * https://cdn.pixabay.com/audio/2025/01/13/audio_b4c259c69e.mp3 - beta waves
    */
   const backgroundMusicRef = useRef<HTMLAudioElement>(
-    new Audio("https://cdn.pixabay.com/audio/2025/01/13/audio_b4c259c69e.mp3")
+    new Audio("https://audiocdn.epidemicsound.com/ES_ITUNES/6EHqvP_Nocturne/ES_Nocturne.mp3")
   );
   // Make the audio loop indefinitely so it restarts once it ends
   backgroundMusicRef.current.loop = true;
